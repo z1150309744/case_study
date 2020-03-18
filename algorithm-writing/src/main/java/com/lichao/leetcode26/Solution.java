96c39e6bb38a8199c06fcc22d4e7022d11ad3757
lichao16 <lichao16@vipkid.com.cn>
2020-03-18 21:01:33 +0800

    Merge branch 'algorithm_writing' of github.com:z1150309744/case_study into algorithm_writing
    # Conflicts:
    #	.gitignore

---
+++ algorithm-writing/src/main/java/com/lichao/leetcode26/Solution.java
@@ -0,0 +1,30 @@
+package com.lichao.leetcode26;
+
+/**
+ * @Author: lichao
+ * @Date: 2020-03-18 20:38
+ */
+class Solution {
+    public static int removeDuplicates(int[] nums) {
+
+        int top = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[top] == nums[i]){
+                continue;
+            }else{
+                nums[++top] = nums[i];
+            }
+        }
+        return top + 1;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        int[] input = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(input));
+
+    }
+}
