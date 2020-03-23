4a8ff73a5e5d57a9ce97e61c6810694a5d316ca5
lichao16 <lichao16@vipkid.com.cn>
2020-03-23 22:21:51 +0800

    Merge branch 'algorithm_writing' of github.com:z1150309744/case_study into algorithm_writing
    # Conflicts:
    #	.gitignore

---
+++ algorithm-writing/src/main/java/com/lichao/P_5.java
@@ -0,0 +1,57 @@
+package com.lichao;
+
+/**
+ * @Author: lichao
+ * @Date: 2020-01-15 22:13
+ */
+public class P_5 {
+    public String longestPalindrome(String s) {
+
+        if (s.isEmpty()) {
+            return "";
+        }
+        char[] input = s.toCharArray();
+        String res = "";
+        int max = 1;
+
+        // 0.1.2.3.4.5.6.7.8;
+        //
+        if (s.isEmpty()) {
+            return "";
+        }
+
+        for (int i = 0; i < input.length; i++) {
+            int temp = 1;
+            int j;
+            for (j = 1; i - j >= 0 && i + j < input.length; j++) {
+                if (input[i - j] == input[i + j]) {
+                    temp = temp + 2;
+                } else {
+                    break;
+                }
+
+            }
+            if (temp > max) {
+                max = temp;
+                res = s.substring(i - j + 1, i + j - 1 + 1);
+            }
+            temp = 0;
+            for (j = 0; i - j >= 0 && i + 1 + j < input.length; j++) {
+                if (input[i - j] == input[i + j + 1]) {
+                    temp += 2;
+                } else {
+                    break;
+                }
+            }
+            if (temp > max) {
+                max = temp;
+                res = s.substring(i - j + 1, i + j + 1);
+            }
+        }
+        if (max == 1)
+            return input[0] + "";
+        return res;
+    }
+
+
+}
