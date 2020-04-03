06fd6802bafbbc3c0636b8f4afa8a2a59d17c4d9
zww245640 <zww245640@alibabapictures.com>
2020-04-03 19:00:21 +0800

    zww:比较版本号

---
+++ algorithm-writing/src/main/java/com/nishui/exception/CheckException.java
@@ -0,0 +1,12 @@
+package com.nishui.exception;
+
+/**
+ * @Description 参数检车类
+ * @Date 2020年04月03日 10时18分
+ * @Created by yiwen
+ */
+public class CheckException extends RuntimeException {
+
+    public CheckException() {
+    }
+}
